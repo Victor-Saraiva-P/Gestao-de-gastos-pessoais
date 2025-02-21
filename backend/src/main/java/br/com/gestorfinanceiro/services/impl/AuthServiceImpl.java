51f1df7a9bd092be93d4770eda8075ffad205cb9
Victor Saraiva <victor_alex2005@hotmail.com>
2025-02-21 15:47:57 -0300

    feat: implementa hash das senhas no register e validação de senha no login - tudo isso em AuthServiceImpl

--- backend/src/main/java/br/com/gestorfinanceiro/services/impl/AuthServiceImpl.java
+++ backend/src/main/java/br/com/gestorfinanceiro/services/impl/AuthServiceImpl.java
@@ -3,25 +3,36 @@ package br.com.gestorfinanceiro.services.impl;
 import br.com.gestorfinanceiro.models.UserEntity;
 import br.com.gestorfinanceiro.repositories.UserRepository;
 import br.com.gestorfinanceiro.services.AuthService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthServiceImpl(UserRepository userRepository) {
+    public AuthServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public UserEntity register(UserEntity userEntity) {
+        //TODO: Criar validação para campos
+        userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
         return userRepository.save(userEntity);
     }
 
     @Override
     public UserEntity login(String email, String password) {
-        return userRepository.findByEmail(email)
+        UserEntity userFoundByEmail = userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (passwordEncoder.matches(password, userFoundByEmail.getPassword())) {
+            return userFoundByEmail;
+        } else {
+            throw new RuntimeException("Invalid password");
+        }
     }
 }
