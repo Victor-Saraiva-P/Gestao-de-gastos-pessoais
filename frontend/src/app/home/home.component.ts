fada897edcdfb5623d0be56b728c39840cc35fd7
isabe1ltenorio <isabel.tenorio@sauter.digital>
2025-02-25 20:11:04 -0300

    feat: cria lógica de logout do sistema e adiciona estilo a página de home

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,17 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <section>
-     <h2>Usuario logado</h2>
-    </section>
+    <nav class="navbar">
+      <div class="nav-container">
+        <div class="left-section">
+        </div>
+        <div class="right-section">
+          <button class="logout" (click)="logout()">Sair</button>
+        </div>
+      </div>
+    </nav>
   `,
   styleUrls: ['./home.component.css']
+  
 })
 export class HomeComponent {
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {}
 
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
