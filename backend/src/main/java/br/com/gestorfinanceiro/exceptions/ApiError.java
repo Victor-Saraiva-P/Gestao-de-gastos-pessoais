9bd1ceb1f371e44081bcefdd62d95ae3fadc9b4b
Victor Saraiva <victor_alex2005@hotmail.com>
2025-02-26 17:54:16 -0300

    feat: adiciona tratamento de erros de validação e atualiza mensagens de exceção

--- backend/src/main/java/br/com/gestorfinanceiro/exceptions/ApiError.java
+++ backend/src/main/java/br/com/gestorfinanceiro/exceptions/ApiError.java
@@ -3,15 +3,21 @@ package br.com.gestorfinanceiro.exceptions;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 public record ApiError(
         LocalDateTime timestamp,
         int status,
         String error,
-        String message
+        String message,
+        Map<String, String> errors // Novo campo para armazenar detalhes dos erros
 ) {
     public ApiError(HttpStatus status, String message) {
-        this(LocalDateTime.now(), status.value(), status.getReasonPhrase(), message);
+        this(LocalDateTime.now(), status.value(), status.getReasonPhrase(), message, null);
+    }
+
+    public ApiError(HttpStatus status, String message, Map<String, String> errors) {
+        this(LocalDateTime.now(), status.value(), status.getReasonPhrase(), message, errors);
     }
 
     public String toJson() {
@@ -20,9 +26,9 @@ public record ApiError(
                     "timestamp": "%s",
                     "status": %d,
                     "error": "%s",
-                    "message": "%s"
+                    "message": "%s",
+                    "errors": %s
                 }
-                """.formatted(this.timestamp, this.status, this.error, this.message);
+                """.formatted(this.timestamp, this.status, this.error, this.message, this.errors);
     }
-
 }
--- backend/src/main/java/br/com/gestorfinanceiro/exceptions/auth/login/InvalidPasswordException.java
+++ backend/src/main/java/br/com/gestorfinanceiro/exceptions/auth/login/InvalidPasswordException.java
@@ -1,7 +1,7 @@
 package br.com.gestorfinanceiro.exceptions.auth.login;
 
 public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException(String message) {
-        super(message);
+    public InvalidPasswordException() {
+        super("Senha inválida");
     }
 }
