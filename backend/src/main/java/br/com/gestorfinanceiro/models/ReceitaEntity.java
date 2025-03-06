ff427cd9b10cb88f6fff4cc3296d4194f9eff95d
Victor Saraiva <victor_alex2005@hotmail.com>
2025-03-06 18:14:51 -0300

    refactor: adiciona um "s" Receita's', renomeia enum ReceitaCategorias para ReceitasCategorias

--- backend/src/main/java/br/com/gestorfinanceiro/models/ReceitaEntity.java
+++ backend/src/main/java/br/com/gestorfinanceiro/models/ReceitaEntity.java
@@ -1,82 +1,94 @@
-    package br.com.gestorfinanceiro.models;
+package br.com.gestorfinanceiro.models;
 
-    import br.com.gestorfinanceiro.models.enums.ReceitaCategorias;
-    import jakarta.persistence.*;
+import br.com.gestorfinanceiro.models.enums.ReceitasCategorias;
+import jakarta.persistence.*;
 
-    import java.math.BigDecimal;
-    import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
-    @Entity
-    @Table(name = "receitas")
-    public class ReceitaEntity {
+@Entity
+@Table(name = "receitas")
+public class ReceitaEntity {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.UUID)
-        private String uuid;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String uuid;
 
-        @Column(nullable = false)
-        private LocalDate data;
+    @Column(nullable = false)
+    private LocalDate data;
 
-        @Enumerated(EnumType.STRING)
-        @Column(nullable = false)
-        private ReceitaCategorias categoria;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ReceitasCategorias categoria;
 
-        @Column(nullable = false, precision = 19, scale = 4)
-        private BigDecimal valor;
+    @Column(nullable = false, precision = 19, scale = 4)
+    private BigDecimal valor;
 
 
-        @Column(nullable = false)
-        private String origemDoPagamento;
+    @Column(nullable = false)
+    private String origemDoPagamento;
 
-        @Column(nullable = false, columnDefinition = "TEXT")
-        private String observacoes;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String observacoes;
 
-        // Getters and Setters
-        public String getUuid() {
-            return uuid;
-        }
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
 
-        public void setUuid(String uuid) {
-            this.uuid = uuid;
-        }
-
-        public LocalDate getData() {
-            return data;
-        }
-
-        public void setData(LocalDate data) {
-            this.data = data;
-        }
-
-        public ReceitaCategorias getCategoria() {
-            return categoria;
-        }
-
-        public void setCategoria(ReceitaCategorias categoria) {
-            this.categoria = categoria;
-        }
-
-        public BigDecimal getValor() {
-            return valor;
-        }
-
-        public void setValor(BigDecimal valor) {
-            this.valor = valor;
-        }
-
-        public String getOrigemDoPagamento() {
-            return origemDoPagamento;
-        }
-
-        public void setOrigemDoPagamento(String origemDoPagamento) {
-            this.origemDoPagamento = origemDoPagamento;
-        }
-
-        public String getObservacoes() {
-            return observacoes;
-        }
-
-        public void setObservacoes(String observacoes) {
-            this.observacoes = observacoes;
-        }
+    // Getters and Setters
+    public String getUuid() {
+        return uuid;
     }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public ReceitasCategorias getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(ReceitasCategorias categoria) {
+        this.categoria = categoria;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getOrigemDoPagamento() {
+        return origemDoPagamento;
+    }
+
+    public void setOrigemDoPagamento(String origemDoPagamento) {
+        this.origemDoPagamento = origemDoPagamento;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+}
