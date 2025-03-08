8889383cba51f7fad8f146d90b5d3c47b041bf16
Clauderson Branco Xavier <xavierclauderson98@gmail.com>
2025-03-08 18:15:46 -0300

    feat: implementação da UI/UX da página de não autorizado

--- frontend/src/app/auth/unauthorized/unauthorized.component.ts
+++ frontend/src/app/auth/unauthorized/unauthorized.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-unauthorized',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      Seu usuário não tem permissão para acessar essa página.
-    </p>
+    <div class="acesso-negado">
+      <h1>Acesso Negado</h1>
+      <p>Você não tem permissão para acessar esta página.</p>
+      <button (click)="home()">Voltar para a página inicial</button>
+    </div>
+
   `,
   styleUrls: ['./unauthorized.component.css']
 })
 export class UnauthorizedComponent {
+  title = "unauthorized"
 
+  private router = inject(Router);
+
+  home() {
+    this.router.navigate(['/home']);
+  }
 }
