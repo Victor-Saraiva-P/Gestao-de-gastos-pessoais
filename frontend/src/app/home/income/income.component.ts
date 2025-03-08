413cc36fa9a77716d56f7a2bfedd75f589c73728
Clauderson Branco Xavier <xavierclauderson98@gmail.com>
2025-03-08 16:02:27 -0300

    feat: implementação da UI da página de receitas

--- frontend/src/app/home/income/income.component.ts
+++ frontend/src/app/home/income/income.component.ts
@@ -12,131 +12,143 @@ import { OnInit } from '@angular/core';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <section>
-      <div>
-        <h2>Create Income</h2>
-        <form [formGroup]="creatIncomeForm" (ngSubmit)="onSubmit()">
-          <!-- Data -->
-          <label for="data">Data</label>
-          <input type="date" formControlName="data" placeholder="Digite a data"/>
-        
-          <!-- Categoria -->
-          <label for="categoria">Categoria</label>
-          <input type="text" formControlName="categoria" placeholder="Digite a categoria"/>
-
-          <!-- Valor -->
-          <label for="valor">Valor</label>
-          <input type="text" formControlName="valor" placeholder="Digite o valor"/>
-
-          <!-- Origem -->
-          <label for="origemDoPagamento">Origem</label>
-          <input type="text" formControlName="origemDoPagamento" placeholder="Digite a origem"/>
-
-          <!-- Observação --> 
-          <label for="">Observação</label>
-          <input type="text" formControlName="observacoes" placeholder="Digite a observação"/>
-
-          <!-- Botão de Submit -->
-          <button type="submit" [disabled]="creatIncomeForm.invalid">Criar receita</button>
-        </form>
+    <section class="expense-container">
+      <button class ="home-button"(click)="home()">home</button>
+      <div class="left-section">
+        <!-- Botões para abrir os modais -->
+        <button (click)="openModal('create')">Criar Receita</button>
+        <button (click)="openModal('edit')">Editar Receita</button> 
+        <button (click)="openModal('remove')">Remover Despesa</button>
       </div>
 
-      <div>
-        <h2>Remove Income</h2>
-        <form [formGroup]="removeIncomeForm" (ngSubmit)="onSubmitRemove()">
-          <!-- Id -->
-          <label for="id">Id</label>
-          <input type="text" formControlName="id" placeholder="Digite o id"/>
+      <div class="main-content">
+      <!-- Lista de Receitas -->
+        <div class="income-list">
+          <h2>Lista de Receitas</h2>
+          <ul>
+            <li *ngFor="let income of incomes">
+              <div>
+                <strong>Data:</strong> {{ income.data | date:'dd/MM/yyyy' }} <br>
+                <strong>Categoria:</strong> {{ income.categoria }} <br>
+                <strong>Valor:</strong> R$ {{ income.valor | number:'1.2-2' }} <br>
+                <strong>Origem do Pagamento:</strong> {{ income.origemDoPagamento }} <br>
+                <strong>Observações:</strong> {{ income.observacoes || 'Nenhuma' }}
+              </div>
+            </li>
+          </ul>
+        </div>
 
-          <!-- Botão de Submit -->
-          <button type="submit" [disabled]="removeIncomeForm.invalid">Remover receita</button>
-        </form>
+      <!-- Modal Criar Receita -->
+        <div [ngClass]="{'modal': true, 'show-modal': modalType === 'create'}">
+        <div class="modal-content">
+          <button class="close" (click)="closeModal()">&times;</button>
+          <h2>Criar Receitas</h2>
+          <form [formGroup]="createIncomeForm" (ngSubmit)="onSubmitCreate()">
+            <label for="data">Data</label>
+            <input type="date" formControlName="data" placeholder="Digite a data"/>
+
+            <label for="categoria">Categoria</label>
+            <div>
+              <select id="categoria" formControlName="categoria" required>
+                <option value="" disabled selected>Selecione uma categoria</option>
+                <option value="SALARIO">Salario</option>
+                <option value="RENDIMENTO_DE_INVESTIMENTO">Rendimento de investimentos</option>
+                <option value="COMISSOES">Comissões</option>
+                <option value="BONUS">Bonus</option>
+                <option value="BOLSA_DE_ESTUDOS">Bolsa de estudos</option>
+              </select>
+            </div>
+
+            <label for="valor">Valor</label>
+            <input type="text" formControlName="valor"/>
+
+            <label for="origemDoPagamento">Origem</label>
+            <input type="text" formControlName="origemDoPagamento"/>
+
+            <label>Observação</label>
+            <input type="text" formControlName="observacoes"/>
+
+            <button type="submit" [disabled]="createIncomeForm.invalid">Criar Receita</button>
+          </form>
+        </div>
+        </div>
+
+        <!-- Modal Editar Despesa -->
+      <div [ngClass]="{'modal': true, 'show-modal': modalType === 'edit'}">
+        <div class="modal-content">
+          <button class="close" (click)="closeModal()">&times;</button>
+          <h2>Editar Despesa</h2>
+          <form [formGroup]="editIncomeForm" (ngSubmit)="onSubmitEdit()">
+            <label>ID</label>
+            <input type="text" formControlName="id"/>
+
+            <label>Data</label>
+            <input type="date" formControlName="data"/>
+
+            <label>Categoria</label>
+            <div>
+              <select id="categoria" formControlName="categoria" required>
+                <option value="" disabled selected>Selecione uma categoria</option>
+                <option value="SALARIO">Salario</option>
+                <option value="RENDIMENTO_DE_INVESTIMENTO">Rendimento de investimentos</option>
+                <option value="COMISSOES">Comissões</option>
+                <option value="BONUS">Bonus</option>
+                <option value="BOLSA_DE_ESTUDOS">Bolsa de estudos</option>
+              </select>
+            </div>
+
+            <label>Valor</label>
+            <input type="number" formControlName="valor"/>
+
+            <label>Origem</label>
+            <input type="text" formControlName="origemDoPagamento"/>
+
+            <label>Observação</label>
+            <input type="text" formControlName="observacoes"/>
+
+            <button type="submit" [disabled]="editIncomeForm.invalid">Salvar Alterações</button>
+          </form>
+        </div>
       </div>
-    
-<div>
-  <h2>Edit Income</h2>
-  <form [formGroup]="editIncomeForm" (ngSubmit)="onSubmitEdit()">
-    <!-- ID -->
-    <label for="id">Id</label>
-    <input type="text" formControlName="id" placeholder="Digite o id" required/>
 
-    <!-- Data -->
-    <label for="data">Data</label>
-    <input type="date" formControlName="data" placeholder="Digite a data"/>
+      <!-- Modal Remover Receita -->
+       <div [ngClass]="{'modal': true, 'show-modal': modalType === 'remove'}">
+        <div class="modal-content">
+          <button class="close" (click)="closeModal()">&times;</button>
+          <h2>Remover Receitas</h2>
+          <form [formGroup]="removeIncomeForm" (ngSubmit)="onSubmitRemove()">
+              <label>Id da Receita</label>
+              <input type="text" formControlName="id"/>
 
-    <!-- Categoria -->
-    <label for="categoria">Categoria</label>
-    <input type="text" formControlName="categoria" placeholder="Digite a categoria"/>
-
-    <!-- Valor -->
-    <label for="valor">Valor</label>
-    <input type="text" formControlName="valor" placeholder="Digite o valor"/>
-
-    <!-- Origem -->
-    <label for="origemDoPagamento">Origem</label>
-    <input type="text" formControlName="origemDoPagamento" placeholder="Digite a origem"/>
-
-    <!-- Observação -->
-    <label for="observacoes">Observação</label>
-    <input type="text" formControlName="observacoes" placeholder="Digite a observação"/>
-
-    <button type="submit" [disabled]="editIncomeForm.invalid">Editar receita</button>
-  </form>
-</div>
-
-<div class="income-list">
-  <h2>Receitas Cadastradas</h2>
-  
-  <table *ngIf="incomes.length > 0">
-    <thead>
-      <tr>
-        <th>Data</th>
-        <th>Categoria</th>
-        <th>Valor</th>
-        <th>Origem</th>
-        <th>Observações</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr *ngFor="let income of incomes">
-        <td>{{ income.data | date:'dd/MM/yyyy' }}</td>
-        <td>{{ income.categoria }}</td>
-        <td>{{ income.valor | currency:'BRL' }}</td>
-        <td>{{ income.origemDoPagamento }}</td>
-        <td>{{ income.observacoes }}</td>
-      </tr>
-    </tbody>
-  </table>
-
-  <p *ngIf="incomes.length === 0">Nenhuma receita cadastrada.</p>
-</div>
-<div class="right-section">
-          <button class="logout" (click)="home()">Home</button>
-      </div>
-</section>
+            <button type="submit" [disabled]="removeIncomeForm.invalid">Remover</button>
+          </form>
+        </div>
+       </div>
+    </div>
+  </section>
   `,
   styleUrls: ['income.component.css']
   
 })
-export class IncomeComponent implements OnInit {
+export class IncomeComponent implements OnInit{
   title = 'income';
-  
+
+  incomes: Income[] = [];
+  modalType: 'create' | 'edit' | 'remove' | null = null;
+
   private homeService = inject(HomeService);
   private router = inject(Router);
   private fb = inject(FormBuilder);
 
-  creatIncomeForm: FormGroup = this.fb.group({
-      data: ['', Validators.required],
-      categoria: ['', Validators.required],
-      valor: ['', Validators.required],
-      origemDoPagamento: ['', Validators.required],
-      observacoes: ['', [Validators.required]],
+  createIncomeForm: FormGroup = this.fb.group({
+    data: ['', Validators.required],
+    categoria: ['', Validators.required],
+    valor: ['', Validators.required],
+    origemDoPagamento: ['', Validators.required],
+    observacoes: ['', [Validators.required]],
   });
 
-  removeIncomeForm: FormGroup = this.fb.group({
-    id: ['', Validators.required],
-  });
-
+  
   editIncomeForm: FormGroup = this.fb.group({
     id: ['', Validators.required],
     data: ['', Validators.required],
@@ -145,62 +157,78 @@ export class IncomeComponent implements OnInit {
     origemDoPagamento: ['', Validators.required],
     observacoes: ['', Validators.required]
   });
-
-    async onSubmit() {
-      if (this.creatIncomeForm.valid) {
-        try {
-          const { data, categoria, valor, origemDoPagamento, observacoes } = this.creatIncomeForm.value;
-          const newIncome: Income = { data, categoria, valor, origemDoPagamento, observacoes };
   
-          await this.homeService.createIncome(newIncome);
-          await this.ngOnInit(); 
-          this.creatIncomeForm.reset(); 
-          this.router.navigate(['/home']);
-        } catch (err) {
-          alert('Error registering income: ' + err);
-        }
-      }
-    }
-    
-    async onSubmitRemove() {
-      if (this.removeIncomeForm.valid) {
-        try {
-          const { id } = this.removeIncomeForm.value;
-          await this.homeService.removeIncome(id);
-          await this.ngOnInit(); 
-          this.removeIncomeForm.reset(); 
-          this.router.navigate(['/home']);
-        } catch (err) {
-          alert('Error removing income: ' + err);
-        }
-      }
-    }
-    
-    async onSubmitEdit() {
-      if (this.editIncomeForm.valid) {
-        try {
-          const { id, data, categoria, valor, origemDoPagamento, observacoes } = this.editIncomeForm.value;
-          const updatedIncome: Income = { data, categoria, valor, origemDoPagamento, observacoes };
-          
-          await this.homeService.editIncome(id, updatedIncome);
-          await this.ngOnInit();
-          this.editIncomeForm.reset(); 
-          this.router.navigate(['/home']);
-        } catch (err) {
-          alert('Error updating income: ' + err);
-        }
-      }
-    }
+  removeIncomeForm: FormGroup = this.fb.group({
+    id: ['', Validators.required],
+  });
 
-  incomes: Income[] = [];
-  async ngOnInit() {
-    try {
-      const incomes = await this.homeService.getIncomes();
-      if (incomes) {
-        this.incomes = incomes;
+  ngOnInit() {
+    this.loadIncomes(); // Chama ao iniciar
+  }
+
+  async loadIncomes() {
+    const response = await this.homeService.getIncomes();
+    if (response) {
+      this.incomes = response; // Atualiza a lista
+    }
+  }
+
+  refreshPage() {
+    window.location.reload();
+  }
+
+
+  openModal(type: 'create' | 'edit' | 'remove') {
+    this.modalType = type;
+  }
+
+  // Fechar o modal
+  closeModal() {
+    this.modalType = null;
+  }
+
+
+  onSubmitCreate() {
+    if (this.createIncomeForm.valid) {
+      const { data, categoria, valor, origemDoPagamento, observacoes } = this.createIncomeForm.value;
+      const newIncome: Income= { data, categoria, valor, origemDoPagamento, observacoes };
+
+      this.homeService.createIncome(newIncome).catch(err => alert('Error registering income: ' + err));
+      this.router.navigate(['/home']).then(() => {
+        alert("Receita criada com sucesso!")
+        this.router.navigate(['/home/income']);
+      })
+      .catch(err => alert('Erro ao criar receita: ' + err));
+      this.refreshPage();
+    }
+  }
+    
+  onSubmitRemove() {
+    if (this.removeIncomeForm.valid) {
+      const {id} = this.removeIncomeForm.value;
+      this.homeService.removeIncome(id).then(() => {
+        alert('Receita removida com sucesso!')
+        this.router.navigate(['/home/income']);
+      })
+      .catch(err => alert('Error removing income: ' + err));
+      this.router.navigate(['/home/income']);
+      this.refreshPage();
+    }
+  } 
+    
+  async onSubmitEdit() {
+    if (this.editIncomeForm.valid) {
+      try {
+        const { id, data, categoria, valor, origemDoPagamento, observacoes } = this.editIncomeForm.value;
+        const updatedIncome: Income = { data, categoria, valor, origemDoPagamento, observacoes };
+        
+        await this.homeService.editIncome(id, updatedIncome);
+        this.router.navigate(['/home/income']);
+        this.refreshPage();
+
+      } catch (err) {
+        alert('Error updating income: ' + err);
       }
-    } catch (error) {
-      console.error('Erro ao carregar receitas:', error);
     }
   }
 
