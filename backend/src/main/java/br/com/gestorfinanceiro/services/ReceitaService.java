1d60ed63e4b63b24de8d3136721cd99b1fff53dc
luisfelipe03 <llipeandrade34@gmail.com>
2025-03-08 13:18:34 -0300

    feat: Adicionando o método de gerar gráfico de pizza do ReceitaService

--- backend/src/main/java/br/com/gestorfinanceiro/services/ReceitaService.java
+++ backend/src/main/java/br/com/gestorfinanceiro/services/ReceitaService.java
@@ -1,8 +1,12 @@
 package br.com.gestorfinanceiro.services;
 
+import br.com.gestorfinanceiro.dto.GraficoPizzaDTO;
 import br.com.gestorfinanceiro.models.ReceitaEntity;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 public interface ReceitaService {
     ReceitaEntity criarReceita(ReceitaEntity receita, String userId);
@@ -14,4 +18,6 @@ public interface ReceitaService {
     ReceitaEntity atualizarReceita(String uuid, ReceitaEntity receitaAtualizada);
 
     void excluirReceita(String uuid);
+
+    GraficoPizzaDTO gerarGraficoPizza(String userId, LocalDate inicio, LocalDate fim);
 }
