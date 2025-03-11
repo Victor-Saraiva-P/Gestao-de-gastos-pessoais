754e7bbae5d11a41682b5e5218696f64ea41cf89
Clauderson Branco Xavier <xavierclauderson98@gmail.com>
2025-03-11 12:29:26 -0300

    refactor: Refatorando templates

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -9,64 +9,60 @@ import { RouterModule } from '@angular/router';
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
-  template: `
+  template:`
     <section class="form-section">
-  <div class="login-container">
-    <div class="title-container">
-      <div>
-        <h2>Entre <br> na sua conta </h2>
-       <!-- <h3>     </h3>-->
+      <div class="login-container">
+        <div class="title-container">
+          <div>
+            <h2>Entre <br> na sua conta </h2>
+          </div>
+          <div class="coins">
+            <img src="assets/coin.svg" />
+          </div>
+        </div>
+
+        <form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
+          <div class="input-container">
+            <label for="email">Email</label>
+            <div class="input-box">
+              <input 
+                type="email" 
+                id="email" 
+                formControlName="email" 
+                placeholder="Teste@gmail.com" 
+                required>
+              <i class="input-icon">
+                <img src="assets/mail-icon.svg" alt="ícone cadeado">
+              </i>
+            </div>
+          </div>
+
+          <div class="input-container">
+            <label for="password">Senha</label>
+            <div class="input-box">
+              <input 
+                [type]="passwordVisible ? 'text' : 'password'"  
+                id="password" 
+                formControlName="password" 
+                placeholder="Digite sua senha" 
+                required>
+              <i class="input-icon" (click)="togglePasswordVisibility('password')" style="cursor: pointer;">
+                <img [src]="passwordVisible ? 'assets/open-lock.svg' : 'assets/lock-icon.svg'" alt="ícone senha">
+              </i>
+            </div>
+          </div>
+          <button class="entrar-bnt" type="submit" [disabled]="loginForm.invalid">Entrar</button>
+        </form>
+        <p>Não tem uma conta?</p>
+        <a [routerLink]="['/register']">
+          <button class="cadastro-btn" type="button">Cadastre-se</button>
+        </a>
       </div>
-      <div class="coins">
-        <img src="assets/coin.svg" />
+
+      <div class="image-container">
+        <img src="assets/logo2-branco.png"/>    
       </div>
-  </div>
-
-<form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
-<div class="input-container">
-  <label for="email">Email</label>
-  <div class="input-box">
-    <input 
-      type="email" 
-      id="email" 
-      formControlName="email" 
-      placeholder="Teste@gmail.com" required>
-    <i class="input-icon">
-      <img src="assets/mail-icon.svg" alt="ícone cadeado">
-    </i>
-  </div>
-</div>
-
-<div class="input-container">
-  <label for="password">Senha</label>
-  <div class="input-box">
-    <input 
-      [type]="passwordVisible ? 'text' : 'password'"  
-      id="password" 
-      formControlName="password" 
-      placeholder="Digite sua senha" 
-      required
-    >
-    <i class="input-icon" (click)="togglePasswordVisibility('password')" style="cursor: pointer;">
-    <img [src]="passwordVisible ? 'assets/open-lock.svg' : 'assets/lock-icon.svg'" alt="ícone senha">
-    </i>
-  </div>
-</div>
-<button class="entrar-bnt" type="submit" click="loginForm.invalid">Entrar</button>
-</form>
-   <p>  Não tem uma conta? </p>
-   <a [routerLink]="['/register']">
-  <button class="cadastro-btn" type="button">Cadastre-se</button>
-</a>
-
-</div>
-
-<div class="image-container">
-  <!--<div><h2>FINANCY</h2></div>-->
-  <img src="assets/logo2-branco.png"/>    
-</div>
-</section>
-
+    </section>
   `,
   styleUrls: ['./login.component.css']
 })
