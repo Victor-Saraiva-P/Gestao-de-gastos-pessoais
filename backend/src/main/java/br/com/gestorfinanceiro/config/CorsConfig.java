436e2145180b53670e3ce2bd1f229463d22c5570
Victor Saraiva <victor_alex2005@hotmail.com>
2025-03-17 11:13:13 -0300

    feat: adiciona origem permitida para CORS no ambiente de produção

--- backend/src/main/java/br/com/gestorfinanceiro/config/CorsConfig.java
+++ backend/src/main/java/br/com/gestorfinanceiro/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://gestao-de-gastos-pessoais.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
