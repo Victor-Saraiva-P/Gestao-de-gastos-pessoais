113e609ffaf7f4c184c146be821e5d29c0e897d5
Victor Saraiva <victor_alex2005@hotmail.com>
2025-03-20 11:25:27 -0300

    feat: melhora das annotations do parâmetro da função doFilterInternal em JwtFilter

--- backend/src/main/java/br/com/gestorfinanceiro/config/security/JwtFilter.java
+++ backend/src/main/java/br/com/gestorfinanceiro/config/security/JwtFilter.java
@@ -29,8 +29,10 @@ public class JwtFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
         String token = recoverToken(request);
 
         if (token != null) {
