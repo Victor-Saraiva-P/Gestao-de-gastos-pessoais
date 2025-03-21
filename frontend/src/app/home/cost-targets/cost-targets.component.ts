cc24f3875945b58e5c2f4ad95e8ffcd3d49d2ab5
Clauderson Branco Xavier <xavierclauderson98@gmail.com>
2025-03-20 23:16:50 -0300

    feat: Criação da página de metas de custo

---
+++ frontend/src/app/home/cost-targets/cost-targets.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cost-targets',
+  imports: [CommonModule],
+  templateUrl: './cost-targets.component.html',
+  styleUrl: './cost-targets.component.css'
+})
+export class CostTargetsComponent {
+
+}
