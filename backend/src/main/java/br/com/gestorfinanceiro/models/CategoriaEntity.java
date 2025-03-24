c3276457d47511b4a7aa5d722c03555bfd2b9000
Victor Saraiva <victor_alex2005@hotmail.com>
2025-03-24 18:55:25 -0300

    feat: adiciona entidade Categoria e enum CategoriaType

---
+++ backend/src/main/java/br/com/gestorfinanceiro/models/CategoriaEntity.java
@@ -0,0 +1,78 @@
+package br.com.gestorfinanceiro.models;
+
+import br.com.gestorfinanceiro.models.enums.CategoriaType;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class CategoriaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String uuid;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CategoriaType tipo;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(nullable = false)
+    private boolean isDefault = false;
+
+    @Column(nullable = false)
+    private boolean isRemovable = true;
+
+    // Getters and Setters
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public CategoriaType getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(CategoriaType tipo) {
+        this.tipo = tipo;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public boolean isDefault() {
+        return isDefault;
+    }
+
+    public void setDefault(boolean aDefault) {
+        isDefault = aDefault;
+    }
+
+    public boolean isRemovable() {
+        return isRemovable;
+    }
+
+    public void setRemovable(boolean removable) {
+        isRemovable = removable;
+    }
+}
