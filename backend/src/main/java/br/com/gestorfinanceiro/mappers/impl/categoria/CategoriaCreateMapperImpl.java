43ac667044fb4886794db21963e2a2f17f3b7cb4
Victor Saraiva <victor_alex2005@hotmail.com>
2025-03-24 19:05:52 -0300

    feat: implementa mapeamento entre CategoriaEntity e CategoriaCreateDTO

---
+++ backend/src/main/java/br/com/gestorfinanceiro/mappers/impl/categoria/CategoriaCreateMapperImpl.java
@@ -0,0 +1,26 @@
+package br.com.gestorfinanceiro.mappers.impl.categoria;
+
+import br.com.gestorfinanceiro.dto.CategoriaCreateDTO;
+import br.com.gestorfinanceiro.mappers.Mapper;
+import br.com.gestorfinanceiro.models.CategoriaEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoriaCreateMapperImpl implements Mapper<CategoriaEntity, CategoriaCreateDTO> {
+    private final ModelMapper modelMapper;
+
+    public CategoriaCreateMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public CategoriaCreateDTO mapTo(CategoriaEntity categoriaEntity) {
+        return modelMapper.map(categoriaEntity, CategoriaCreateDTO.class);
+    }
+
+    @Override
+    public CategoriaEntity mapFrom(CategoriaCreateDTO categoriaCreateDTO) {
+        return modelMapper.map(categoriaCreateDTO, CategoriaEntity.class);
+    }
+}
