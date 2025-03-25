20b323b5c15086f84d5b83f1dfb9ab6c1f04ff27
Victor Saraiva <victor_alex2005@hotmail.com>
2025-03-24 21:13:32 -0300

    feat: adiciona método para encontrar categoria por nome, tipo e id do usuário

--- backend/src/main/java/br/com/gestorfinanceiro/repositories/CategoriaRepository.java
+++ backend/src/main/java/br/com/gestorfinanceiro/repositories/CategoriaRepository.java
@@ -1,10 +1,14 @@
 package br.com.gestorfinanceiro.repositories;
 
 import br.com.gestorfinanceiro.models.CategoriaEntity;
+import br.com.gestorfinanceiro.models.enums.CategoriaType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CategoriaRepository extends JpaRepository<CategoriaEntity, String> {
     List<CategoriaEntity> findAllByUserUuid(String userId);
+
+    // Encontrar categoria por nome, tipo, e id do usuário
+    CategoriaEntity findByNomeAndTipoAndUserUuid(String nome, CategoriaType tipo, String userId);
 }
