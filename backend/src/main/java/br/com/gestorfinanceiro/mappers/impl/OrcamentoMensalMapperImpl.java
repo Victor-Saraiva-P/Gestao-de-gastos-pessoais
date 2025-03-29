6b302f14deab9ec88ba1ae185193bdd8aa217787
luisfelipe03 <llipeandrade34@gmail.com>
2025-03-29 09:13:24 -0300

    refactor: Refatorando o mapper para receber CategoriaEntity

--- backend/src/main/java/br/com/gestorfinanceiro/mappers/impl/OrcamentoMensalMapperImpl.java
+++ backend/src/main/java/br/com/gestorfinanceiro/mappers/impl/OrcamentoMensalMapperImpl.java
@@ -2,8 +2,9 @@ package br.com.gestorfinanceiro.mappers.impl;
 
 import br.com.gestorfinanceiro.dto.OrcamentoMensal.OrcamentoMensalDTO;
 import br.com.gestorfinanceiro.mappers.Mapper;
+import br.com.gestorfinanceiro.models.CategoriaEntity;
 import br.com.gestorfinanceiro.models.OrcamentoMensalEntity;
-import br.com.gestorfinanceiro.models.enums.DespesasCategorias;
+import br.com.gestorfinanceiro.repositories.CategoriaRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
@@ -11,22 +12,28 @@ import org.springframework.stereotype.Component;
 public class OrcamentoMensalMapperImpl implements Mapper<OrcamentoMensalEntity, OrcamentoMensalDTO> {
 
     private final ModelMapper modelMapper;
+    private final CategoriaRepository categoriaRepository; // Adicionando o repositório
 
-    public OrcamentoMensalMapperImpl(ModelMapper modelMapper) {
+    public OrcamentoMensalMapperImpl(ModelMapper modelMapper, CategoriaRepository categoriaRepository) {
         this.modelMapper = modelMapper;
+        this.categoriaRepository = categoriaRepository;
     }
 
     @Override
     public OrcamentoMensalDTO mapTo(OrcamentoMensalEntity orcamentoMensalEntity) {
         OrcamentoMensalDTO dto = modelMapper.map(orcamentoMensalEntity, OrcamentoMensalDTO.class);
-        dto.setCategoria(orcamentoMensalEntity.getCategoria().name());
+        dto.setCategoria(orcamentoMensalEntity.getCategoria().getNome());
         return dto;
     }
 
     @Override
     public OrcamentoMensalEntity mapFrom(OrcamentoMensalDTO orcamentoMensalDTO) {
         OrcamentoMensalEntity entity = modelMapper.map(orcamentoMensalDTO, OrcamentoMensalEntity.class);
-        entity.setCategoria(DespesasCategorias.valueOf(orcamentoMensalDTO.getCategoria()));
+
+        CategoriaEntity categoria = categoriaRepository.findByNome(orcamentoMensalDTO.getCategoria())
+                .orElseThrow(() -> new IllegalArgumentException("Categoria não encontrada: " + orcamentoMensalDTO.getCategoria()));
+
+        entity.setCategoria(categoria);
         return entity;
     }
 }
