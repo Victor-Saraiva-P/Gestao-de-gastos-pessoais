441a7c70c6f022daa08b192486f05699fa9cd652
Clauderson Branco Xavier <xavierclauderson98@gmail.com>
2025-03-29 18:20:23 -0300

    feat: implentação dos botões da página de criação de categorias personalizadas para despesas

--- frontend/src/app/home/custom-category/expense/custom-category-expense.component.ts
+++ frontend/src/app/home/custom-category/expense/custom-category-expense.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-custom-category-expense',
@@ -7,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './custom-category-expense.component.css'
 })
 export class CustomCategoryExpenseComponent {
+  title = 'category-expense';
 
+  isRemoving = false;
+  isEditing = false;
+
+  private router = inject(Router);
+
+
+  toggleRemoveMode() {
+    this.isRemoving = !this.isRemoving;
+    if (this.isEditing) {
+      this.isRemoving = false;
+    }
+  }
+
+  toggleEditMode() {
+    this.isEditing = !this.isEditing;
+    if (this.isRemoving) {
+      this.isEditing = false;
+    }
+  }
+
+  expense() {
+    this.router.navigate(['/home/expense']);
+  }
 }
