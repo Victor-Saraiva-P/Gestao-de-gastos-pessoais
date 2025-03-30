6e83fc6ceeaa0cf7afbd62f7e4b47e22a4204343
Clauderson Branco Xavier <xavierclauderson98@gmail.com>
2025-03-30 11:54:29 -0300

    feat: implementando a lógica para criar a categoria personalizada de receita

--- frontend/src/app/home/custom-category/income/custom-category-income.component.ts
+++ frontend/src/app/home/custom-category/income/custom-category-income.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { CustomCategoryService } from '../custom-category.service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-custom-category',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: 'custom-category-income.component.html',
   styleUrls: ['custom-category-income.component.css']
 })
@@ -12,9 +15,23 @@ export class CustomCategoryIncomeComponent {
   
     isRemoving = false;
     isEditing = false;
+    modalType: 'create' | 'edit' | null = null;
   
     private router = inject(Router);
+    private customCategoryService = inject(CustomCategoryService);
+    private fb = inject(FormBuilder);
+
+    createCategoryincomeForm: FormGroup = this.fb.group({
+        name: ['', Validators.required],
+    });
+
+    openModal(type: 'create' | 'edit') {
+      this.modalType = type;
+    }
   
+    closeModal() {
+      this.modalType = null;
+    }
   
     toggleRemoveMode() {
       this.isRemoving = !this.isRemoving;
@@ -33,4 +50,23 @@ export class CustomCategoryIncomeComponent {
     income() {
       this.router.navigate(['/home/income']);
     }
+
+    refreshPage() {
+      window.location.reload();
+    }
+
+    onSubmitCreate() {
+        if (this.createCategoryincomeForm.valid) {
+          const nome: string = this.createCategoryincomeForm.value.name;
+      
+    
+          this.customCategoryService
+            .createCategories('RECEITAS', nome.toUpperCase())
+            .then(() => {
+              alert('Categoria criada com sucesso!');
+              this.refreshPage();
+            })
+            .catch((err) => alert('Erro ao criar Categoria: ' + err));
+        }
+      }
 }
