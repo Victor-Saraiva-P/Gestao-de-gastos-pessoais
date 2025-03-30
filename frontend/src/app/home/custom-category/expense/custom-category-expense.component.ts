21dca865212ce4d73c785c2bc5a62b6e8f64e5cd
Clauderson Branco Xavier <xavierclauderson98@gmail.com>
2025-03-30 12:03:44 -0300

    feat: implementação da lógica para criara a categoria personalizada de despesas

--- frontend/src/app/home/custom-category/expense/custom-category-expense.component.ts
+++ frontend/src/app/home/custom-category/expense/custom-category-expense.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CustomCategoryService } from '../custom-category.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-custom-category-expense',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './custom-category-expense.component.html',
   styleUrl: './custom-category-expense.component.css'
 })
@@ -12,8 +15,24 @@ export class CustomCategoryExpenseComponent {
 
   isRemoving = false;
   isEditing = false;
+  modalType: 'create' | 'edit' | null = null;
 
   private router = inject(Router);
+  private customCategoryService = inject(CustomCategoryService);
+  private fb = inject(FormBuilder);
+
+  createCategoryExpenseForm: FormGroup = this.fb.group({
+      name: ['', Validators.required],
+  });
+
+
+  openModal(type: 'create' | 'edit') {
+    this.modalType = type;
+  }
+
+  closeModal() {
+    this.modalType = null;
+  }
 
 
   toggleRemoveMode() {
@@ -32,5 +51,23 @@ export class CustomCategoryExpenseComponent {
 
   expense() {
     this.router.navigate(['/home/expense']);
+  }
+
+  refreshPage() {
+    window.location.reload();
+  }
+
+  onSubmitCreate() {
+      if (this.createCategoryExpenseForm.valid) {
+        const nome: string = this.createCategoryExpenseForm.value.name;
+    
+        this.customCategoryService
+          .createCategories('DESPESAS', nome.toUpperCase())
+          .then(() => {
+            alert('Categoria criada com sucesso!');
+            this.refreshPage();
+          })
+          .catch((err) => alert('Erro ao criar Categoria: ' + err));
+      }
   }
 }
